625496e797a0accead2ffe85ffc659afcb6240be
kepich <pav007prikhod@mail.ru>
2022-05-19 23:12:27 +0700

    Test commit

--- src/main/java/com/backend/scribble/ScribbleApplication.java
+++ src/main/java/com/backend/scribble/ScribbleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScribbleApplication {
 
 	public static void main(String[] args) {
+		// Test commit
 		SpringApplication.run(ScribbleApplication.class, args);
 	}
 
